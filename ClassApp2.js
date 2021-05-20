407ad454010be007d4ad2111700b6609e9ff20c8
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-05-20 21:12:31 +0300

    add ClassApp Component from Class-based Component practice 2

---
+++ ClassApp2.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+// Given a stateless functional component, add state to it
+// state should have a property called `isLoggedIn` which is a boolean
+// (true if logged in, false if not)
+// Then, give your best shot at rendering the word "in" if the user is logged in
+// or "out" if the user is logged out.
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoggedIn: true
+        }
+    }
+    
+    render() {
+        let wordLogged
+        if (this.state.isLoggedIn) {wordLogged = "in"}
+        else {wordLogged = "out"}
+        
+        return (
+            <div>
+                <h1>You are currently logged {wordLogged}</h1>
+            </div>
+        )    
+    }
+}
+
+export default App
