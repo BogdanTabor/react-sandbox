b3979946ae12c828476b657b80554291d609528a
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-05-22 00:44:25 +0300

    add SetState Component from precticing of setState creating

---
+++ SetStateApp.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+        this.countOne = this.countOne.bind(this)
+        this.countTwice = this.countTwice.bind(this)
+
+    }
+ 
+    countOne() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        })
+    }
+     
+    countTwice() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 2
+            }
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.countOne}>Count one</button>
+                <button onClick={this.countTwice}>Count twice</button>
+            </div>
+        )
+    }
+}
+
+export default App
