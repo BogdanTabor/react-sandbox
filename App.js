104615e5123ae07eca49ebdc82f868716173b844
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-05-23 23:14:53 +0300

    fix TodoApp component

--- App.js
+++ App.js
@@ -29,7 +29,10 @@
          this.setState(prevState => {
              const updatedTodos = prevState.todos.map(todo => {
                  if(todo.id === id) {
-                     todo.completed = !todo.completed
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    } 
                  }
                  return todo
              })
