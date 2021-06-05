a080608269321d5bec62cbb15c20f87990729ff2
BogdanTabor <bogdan.tabor@gmail.com>
2021-06-05 22:36:25 +0300

    add useState useEffect Components from scrimba react tutorial lessons 53-56

---
+++ ScrimbaReatTutorial/useState-useEffect/useEffect.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from "react"
+import randomcolor from "randomcolor"
+
+function App() {
+    const [count, setCount] = useState(0)
+    const [color, setColor] = useState("")
+    
+    function increment() {
+        setCount(prevCount => prevCount + 1)
+    }
+    
+    function decrement() {
+        setCount(prevCount => prevCount - 1)
+    }
+    
+    useEffect(() => {
+        setColor(randomcolor())
+    }, [count])
+    
+    //OR USE
+    //const [color, setColor] = useState(randomcolor())
+    //TO RANDOM COLOR ONCE
+    
+    return (
+        <div>
+            <h1 style={{color: color}}>{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
+
+export default App
